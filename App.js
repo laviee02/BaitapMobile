d1b1076afe1cb6f2916b085ba4a383f62f2769bb
VietTran <vn@741523.com>
2022-09-08 15:52:01 +0700

    first commit

--- App.js
+++ App.js
@@ -4,8 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.green}>
+          <Text>Square 1</Text>
+          <StatusBar style="auto" />
+      </View>
+      <View style={styles.red}>
+          <Text>Square 2</Text>
+          <StatusBar style="auto" />
+      </View>
+      <View style={styles.blue}>
+          <Text>Square 3</Text>
+          <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +23,66 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection:'row',
+    backgroundColor: 'white',
     alignItems: 'center',
+    justifyContent: 'space-around' ,
+  },
+
+  green: {
+    width: 100,
+    height: 100,
+    backgroundColor: "lightgreen",
     justifyContent: 'center',
+    alignItems:'center',
+  },
+
+  red: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+
+  blue: {
+    width: 100,
+    height: 100,
+    backgroundColor: "lightblue",
+    justifyContent: 'center',
+    alignItems:'center',
   },
 });
+
+// import React, { Component } from 'react';
+// import { Button, StyleSheet, View } from 'react-native';
+
+// export default class ButtonBasics extends Component {
+//   _onPressButton() {
+//     alert('Hello!')
+//   }
+  
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.buttonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="Press Me"
+//             color='lightgreen'
+//           />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//    flex: 1,
+//    justifyContent: 'center',
+//   },
+//   buttonContainer: {
+//     margin: 20,
+//   },
+// });
